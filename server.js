6eb6e78fe351bf194a4dd526d64fea45da79fbec
mey21009 <mey2100@byui.edu>
2024-02-22 17:19:39 -0700

    shayla's

--- server.js
+++ server.js
@@ -10,4 +10,5 @@ app.get('/nate', (req,res) => {res.send("Hello Nate")});
 app.get('/dallas', (req,res) => {res.send("Hello Dallas")}); 
 app.get('/dani', (req,res) => {res.send("Hello Dani")});
 app.get('/chris', (req,res) => {res.send("Hello Chris")});
+app.get('/shayla', (req,res) => {res.send("Hello Shayla")});
 app.listen(port, ()=>console.log("Listening"));
