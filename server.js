6a3484bc9e43a509a9f51c8c3f9e5e6c48637264
Mechman27 <drbedfo@yahoo.com>
2024-02-22 17:15:24 -0700

    Dallas's Update

--- server.js
+++ server.js
@@ -7,5 +7,6 @@ const app = express();
 app.get('/', (req,res) => {res.send("Hello Browser")});
 
 app.get('/nate', (req,res) => {res.send("Hello Nate")});
+app.get('/sean', (req,res) => {res.send("Hello Dallas")}); 
 
 app.listen(port, ()=>console.log("Listening"));
