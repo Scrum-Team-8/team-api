ae85a0842e9a5297b5bda921201059be039a16ad
stewartd <ste21078@byui.edu>
2024-02-22 17:16:48 -0700

    danis changes

--- server.js
+++ server.js
@@ -7,6 +7,6 @@ const app = express();
 app.get('/', (req,res) => {res.send("Hello Browser")});
 
 app.get('/nate', (req,res) => {res.send("Hello Nate")});
-app.get('/sean', (req,res) => {res.send("Hello Dallas")}); 
-
+app.get('/dallas', (req,res) => {res.send("Hello Dallas")}); 
+app.get('/dani', (req,res) => {res.send("Hello Dani")}); 
 app.listen(port, ()=>console.log("Listening"));
