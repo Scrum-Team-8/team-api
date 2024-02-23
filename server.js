4ae2f5a1753d313ee3d7c8ea87925f154156f2d0
Christian <crissbritez70@gmail.com>
2024-02-22 21:18:00 -0300

    chris code added

--- server.js
+++ server.js
@@ -8,5 +8,6 @@ app.get('/', (req,res) => {res.send("Hello Browser")});
 
 app.get('/nate', (req,res) => {res.send("Hello Nate")});
 app.get('/dallas', (req,res) => {res.send("Hello Dallas")}); 
-app.get('/dani', (req,res) => {res.send("Hello Dani")}); 
+app.get('/dani', (req,res) => {res.send("Hello Dani")});
+app.get('/chris', (req,res) => {res.send("Hello Chris")});
 app.listen(port, ()=>console.log("Listening"));
