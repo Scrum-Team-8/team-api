7596a0114a8e4beedc0fcb63dfa55b86608e0fdf
Christian <crissbritez70@gmail.com>
2024-02-29 21:16:07 -0300

    chris update

---
+++ test/chris.test.js
@@ -0,0 +1,6 @@
+it ('Should return Hello', async ()=> {
+    const response = await fetch('http://localhost:3000/chris');
+    const name = await response.text();
+    expect(name).toBe("chris");
+
+})
