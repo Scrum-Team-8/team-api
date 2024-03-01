03ae4c709e2adf8ff146906e3e78201bfb82d58a
Christian <crissbritez70@gmail.com>
2024-03-01 00:04:06 -0300

    update

--- test/dani.test.js
+++ test/dani.test.js
@@ -1,6 +1,6 @@
 it ('Should return Hello', async ()=> {
-    const response = await fetch('http://localhost:3000/chris');
+    const response = await fetch('http://localhost:3000/dani');
     const name = await response.text();
-    expect(name).toBe("chris");
+    expect(name).toBe("dani");
 
 })
